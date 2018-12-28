bcbe595a2d10ed1d295f9dc9756a65ae6935752c
Andy Mihaja <tyranosorus4204bfmv@gmail.com>
2018-12-28 11:16:02 +0300

    Donwload fonctionel (chemin /target/snapshot....)

--- src/main/java/nopacks/projet/controller/ChansonController.java
+++ src/main/java/nopacks/projet/controller/ChansonController.java
@@ -1,5 +1,12 @@
 package nopacks.projet.controller;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import nopacks.projet.modeles.Chanson;
 import nopacks.projet.services.ChansonService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,6 +23,8 @@ import org.springframework.web.servlet.ModelAndView;
 public class ChansonController {
 
     private ChansonService personService;
+@Autowired
+        ServletContext context;
 
     @Autowired(required = true)
     @Qualifier(value = "chansonService")
@@ -45,6 +54,53 @@ public class ChansonController {
         model.addObject("hira", this.personService.findChansonById(id));
         model.addObject("listChansons", this.personService.listChansons());
         return model;
+    }
+
+    @RequestMapping(value = "download/{nom}", method = RequestMethod.GET)
+
+    public void downloadTest(HttpServletRequest request, HttpServletResponse response, @PathVariable("nom") String nom) throws IOException {
+
+        // get absolute path of the application
+       // ServletContext context = request.getServletContext();
+        String appPath = context.getRealPath("");
+        System.out.println("appPath = " + appPath);
+ 
+        // construct the complete absolute path of the file
+        String fullPath = appPath +"/"+ nom;      
+        File downloadFile = new File(fullPath);
+        FileInputStream inputStream = new FileInputStream(downloadFile);
+         
+        // get MIME type of the file
+        String mimeType = context.getMimeType(fullPath);
+        if (mimeType == null) {
+            // set to binary type if MIME mapping not found
+            mimeType = "application/octet-stream";
+        }
+        System.out.println("MIME type: " + mimeType);
+ 
+        // set content attributes for the response
+        response.setContentType(mimeType);
+        response.setContentLength((int) downloadFile.length());
+ 
+        // set headers for the response
+        String headerKey = "Content-Disposition";
+        String headerValue = String.format("attachment; filename=\"%s\"",
+                downloadFile.getName());
+        response.setHeader(headerKey, headerValue);
+ 
+        // get output stream of the response
+        OutputStream outStream = response.getOutputStream();
+ 
+        byte[] buffer = new byte[512];
+        int bytesRead = -1;
+ 
+        // write bytes read from the input stream into the output stream
+        while ((bytesRead = inputStream.read(buffer)) != -1) {
+            outStream.write(buffer, 0, bytesRead);
+        }
+ 
+        inputStream.close();
+        outStream.close();
     }
 
     @RequestMapping("/delete/{id}")
